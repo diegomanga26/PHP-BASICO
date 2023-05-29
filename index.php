ba5bdc449b456e58238414b496a9af06ea9fa1c2
diegomanga26 <diegoalexandermanga@gmail.com>
2023-05-29 17:19:38 -0500

    feat: :sparkles: Tipos de datos
    Se hace una breve explicacion de cada uno de los tipos de datos.

--- index.php
+++ index.php
@@ -49,5 +49,42 @@
      * *Declarando una constante booleana:
      */
     define("ES_VALIDO", true);
+    /**
+     * ?TIPOS DE DATOS:
+     */
+    /**
+     * *Enteros(int): Se usa para para almacenar números enteros:
+     */
+    $numero = 200;
+    var_dump($numero);
+    /**
+     * *Punto flotante(float): Se usa para almacenar números decimales:
+     */
+    echo '<br></br>';
+    $float = 200.8;
+    var_dump($float);
+    /**
+     * *Cadenas de texto(string): Se usa para almacenar texto:
+     */
+    echo '<br></br>';
+    $nombre1 = "Alexander";
+    var_dump($nombre1);
+    /**
+     * *Booleanos(bool): Se usa para almacenar datos true o false:
+     */
+    echo '<br></br>';
+    $logueado = true;
+    var_dump($logueado);
+    /**
+     * *Arreglos(array): Se usa para almacenar un conjunto de datos:
+     */
+    echo '<br></br>';
+    $array = [];
+    var_dump($array);
+    /**
+     * *Objetos(object): Se usa para almacenar objetos de clases.
+     * *Recursos(resource): Se usa para almacenar referencias o recursos externos, como archivos o conexiones.
+     * *Nulo(null): Se usan para almacenar variables sin valor.
+     */
 
 ?>
