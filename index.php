ec24a5c9245479b0a11902839cc97288be5e0ca9
aaaa <aaa>
2023-05-29 16:57:17 -0500

    feat: :sparkles: Funciones de salida
    Agregando funciones de salida echo, print y sprintf.

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-
+    /**
+     * *FUNCIONES DE SALIDA:
+     */
+    /**
+     * *Función de salida "echo()":
+     */
+    echo '<h1>Hola mundo cruel</h1>';
+    /**
+     * *Función de salida "print()":
+     */
+    $texto = "Mundo";
+    printf("Hola %s", $texto);
+    /**
+     * *Función de salida "sprintf()":
+     */
+    echo '<br></br>';
+    $mensaje = sprintf("Hola %s", $texto);
+    echo $mensaje;
 ?>
