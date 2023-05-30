a1ee5106c98d6675924bafc5286124309d5e4f40
diegomanga26 <diegoalexandermanga@gmail.com>
2023-05-29 22:45:29 -0500

    feat: :sparkles: Numeros y operadores
    Se agrega al codigo los distintos numeros y operadores utilizados en PHP, ademas de los metodos para manipular cadenas.

--- index.php
+++ index.php
@@ -106,10 +106,10 @@
      * *Operadores de matriz:
      * * + = Unión.
      * * == = Igualdad.
-     * * === = Identidad (Cumplir con 3 condiciones).
+     * * === = Identidad (Cumplir con 2 condiciones).
      * * != = Desigualdad.
      * * <> = Desigualdad.
-     * * !== = No identidad (No cumplir con 3 condiciones).
+     * * !== = No identidad (No cumplir con 2 condiciones).
      */
     /**
      * *Operador Bit a Bit:
@@ -120,6 +120,133 @@
      * * >> = Desplazar a la derecha.
      */
     /**
-     * *
+     * *Operador de comparación:
+     * * == = Igualdad.
+     * * === = Identico (Se cumple condicion de valor y tipo de variable).
+     * * != = Desigualdad.
+     * * !== = Totalmente desigual.
+     * * <> = Desigualdad.
+     * * < = Menor que.
+     * * <= = Menor o igual que.
+     * * > = Mayor que.
+     * * >= = Mayor o igual que.
+     * * <=> = Spaceship operator.
      */
+    /**
+     * *Operador de ejecución:
+     * * `` = Acentos graves.
+     */
+    /**
+     * *Operador de control de errores:
+     * *@ = A la señal.
+     */
+    /**
+     * *Operadores de incremento y decremento:
+     * *++$a = PreIncremento.
+     * *$a++ = PostIncremento.
+     * *--$a = PreDecremento.
+     * *$a-- = PostDecremento.
+     */
+    /**
+     * *Operadores Lógicos:
+     * *&& = "Y".
+     * *|| = "o".
+     * *! = "No".
+     * *and = "Y".
+     * *xor = xor.
+     * *or = "o".
+     */
+    /**
+     * *EJEMPLOS DE OPERADORES:
+     */
+    $numero1 = 20;
+    $numero2 = 30;
+    $numero3 = 30;
+    $numero4 = "30";
+
+    /**
+     * *Operador Mayor y Menor que:
+     */
+    echo '<br></br>';
+    var_dump($numero1 > $numero2);
+    echo '<br></br>';
+    var_dump($numero1 < $numero2);
+    echo '<br></br>';
+    /**
+     * *Operador Mayor o igual y Menor o igual que:
+     */
+    var_dump($numero1 >= $numero2);
+    echo '<br></br>';
+    var_dump($numero1 <= $numero2);
+    echo '<br></br>';
+    /**
+     * *Operador de comparación normal:
+     */
+    var_dump($numero2 == $numero3);
+    echo '<br></br>';
+    var_dump($numero3 == $numero4);
+    echo '<br></br>';
+    /**
+     * *Operador de comparación estricto:
+     */
+    var_dump($numero2 === $numero4);
+    echo '<br></br>';
+    /**
+     * *Operador Spaceship:
+     * *Si el valor de la izquierda es menor será -1, si es igual será 0 y si es mayor será 1.
+     */
+    var_dump($numero1 <=> $numero2);
+    echo '<br></br>';
+    var_dump($numero2 <=> $numero3);
+    echo '<br></br>';
+    var_dump($numero2 <=> $numero1);
+    echo '<br></br>';
+    /**
+     * ?METODOS PHP PARA MANIPULAR CADENAS DE CARACTERES:
+     */
+    $nombreCliente = "Campers Campuslands";
+
+    /**
+     * *Conocer toda la extensión de un string:
+     */
+    echo strlen($nombreCliente);
+    echo '<br></br>';
+    var_dump($nombreCliente);
+    /**
+     * *Eliminar espacios en blanco en el string:
+     */
+    $texto1 = trim($nombreCliente);
+    echo '<br></br>';
+    echo strlen($texto1);
+    /**
+     * *Convertir string en mayusculas:
+     */
+    echo strtoupper($nombreCliente);
+    echo '<br></br>';
+    /**
+     * *Convertir string en minusculas:
+     */
+    echo strtolower($nombreCliente);
+
+    $mail1 = "correo@correo.com";
+    $mail2 = "Correo@correo.com";
+
+    var_dump(strtolower($mail1) === strtolower($mail2));
+    echo '<br></br>';
+    echo str_replace('Juan', 'J', $nombreCliente);
+
+    /**
+     * *Revisar si un string existe o no existe:
+     */
+    echo strpos($nombreCliente, 'Pedro');
+
+    $tipoCliente = "Premium - Empresarial";
+
+    echo '<br></br>';
+
+    echo "El Cliente " . $nombreCliente . " es " . $tipoCliente;
+
+    echo '<br></br>';
+
+    echo "El Cliente {$nombreCliente} es ${tipoCliente}"
 ?>
