7a2c2e7c6318ee73a7aef16cbf3d67da5e66fda9
diegomanga26 <diegoalexandermanga@gmail.com>
2023-06-01 18:01:05 -0500

    feat: :sparkles: Actualizando información
    Actualizando información sobre métodos aplicables a los array en php.

--- index.php
+++ index.php
@@ -248,5 +248,193 @@
 
     echo '<br></br>';
 
-    echo "El Cliente {$nombreCliente} es ${tipoCliente}"
+    echo "El Cliente {$nombreCliente} es ${tipoCliente}";
+
+    /**
+     * ?ARREGLOS, ARREGLOS ASOCIATIVOS Y FUNCIONES PARA ARREGLOS:
+     */
+    /**
+     * *Declaración de variables de tipo array indexadas.
+     * @param $nombres
+     */
+    $nombres = array("Diego", "Roberto", "Ivan");
+    var_dump($nombres);
+    echo "<hr>";
+    /**
+     * *Declaración de variables de tipo array multidimensional.
+     * @param $edades, $personas
+     */
+    /**
+     * ? En los array multidimensionales el símbolo "=>" es meramente de asignación.
+     * ? Se indica fuera de parentesis que es de tipo array y dentro los valores.
+     */
+    $edades = array(
+        "Diego" => 23, 
+        "Roberto" => 24, 
+        "Ivan" => 23);
+    var_dump($edades);
+    echo "<hr>";
+    $personas = array(
+        "Diego" => array("Edad" => 23, "Ciudad" => "Valledupar", "País" => "Colombia"),
+        "Roberto" => array("Edad" => 24, "Ciudad" => "Valledupar", "País" => "Colombia"),
+        "Ivan" => array("Edad" => 23, "Ciudad" => "La Jagua de Ibirico", "País" => "Colombia")
+    );
+    print_r($personas);
+    echo "<hr>";
+    /**
+     * ? Para la asignación de datos se llama el array y entre corchetes se va accediendo a la información.
+     */
+    print_r($personas["Diego"]["Ciudad"]);
+    echo "<hr>";
+    /**
+     * * 1) Método array_flip():
+     * * Este método es utilizado para intercambiar las llaves con sus valores y entrega un array nuevo.
+     * ? Podría renombrarse como nuevo array en una variable nueva.
+     */
+
+    $edadesFlip = array_flip($edades);
+    print_r($edadesFlip);
+    echo "<hr>";
+
+    /**
+     * * 2) Método array_fill():
+     * * Este método es utilizado para rellenar un array en un rango de pocisiones, se le puede dar un valor cualquiera que
+     * * se va a repetir a lo largo del array en el rango indicado.
+     */
+
+    $nuevoArray = array_fill(0,8,"Papa");
+    print_r($nuevoArray);
+    echo "<hr>";
+
+    /**
+     * * 3) Método array_filter():
+     * * Este método es utilizado para filtrar elementos en un array recorriendolo y pasandolos a una función "callback".
+     */
+    /**
+     * !MÉTODO CLÁSICO:
+     */
+
+    // $numeritos = array(1,2,3,4,5,6,7,8,9);
+    // function esImpar($numeritos) {
+    //     return $numeritos % 2 != 0;
+    // };
+    // $numeritosImpares = array_filter($numeritos, "esImpar");
+    // print_r($numeritosImpares);
+    /**
+     * ?Otra forma de hacerlo pero con función flecha:
+     */
+    $numeritos = array(1,2,3,4,5,6,7,8,9);
+    $numeritosImpares = array_filter($numeritos,fn($numeritos) => 
+        $numeritos % 2 != 0
+    );
+    print_r($numeritosImpares);
+    echo "<hr>";
+
+    /**
+     * * 4) Método array_map():
+     * * Este método es utilizado para aplicar una función a cada elemento del array y devolviendo un nuevo array con los resultados.
+     */
+    /**
+     * !MÉTODO CLÁSICO:
+     */
+
+    // function dividirEnDos($numeritosImpares) {
+    //     return $numeritosImpares * 2;
+    // };
+
+    // $numeritosDivididos = array_map("dividirEnDos", $numeritosImpares);
+
+    // print_r($numeritosDivididos);
+
+    /**
+     * ?Otra forma de hacerlo pero con función flecha:
+     */
+
+    $numeritosDivididos = array_map(fn($numeritosImpares) => 
+    $numeritosImpares/2,$numeritosImpares);
+    
+    print_r($numeritosDivididos);
+    echo "<hr>";
+    /**
+     * * 5) Método array_reduce():
+     * * Este método es utilizado para reducir el array a un solo valor usando una función "callback", esto lo hace iterando el array.
+     */
+    /**
+     * !MÉTODO CLÁSICO:
+     */
+
+    // function sumar($sumatoria, $numeritosDivididos) {
+    //     return $sumatoria + $numeritosDivididos;
+    // };
+
+    // $total = array_reduce($numeritosDivididos, "sumar");
+
+    // print_r($total);
+
+    /**
+     * ?Otra forma de hacerlo pero con función flecha:
+     */
+
+    $total = array_reduce($numeritosDivididos, fn($sumatoria, $numeritosDivididos) => 
+    $sumatoria + $numeritosDivididos);
+
+    print_r($total);
+    echo "<hr>";
+
+    /**
+     * * 6) Método array_key_exists():
+     * * Este método es utilizado para verificar una llave en un array.
+     */
+
+    if (array_key_exists("Ciudad",$personas["Diego"])) {
+        echo "La clave 'Ciudad' si existe";
+    } else {
+        echo "La clave 'Ciudad' no existe";
+    };
+    echo "<hr>";
+
+    /**
+     * * 7) Método in_array():
+     * * Este método es utilizado para comprobar si un valor existe en un array.
+     */
+
+    if (in_array("Valledupar", $personas["Diego"])) {
+        echo "La ciudad 'Valledupar' existe en el array";
+    } else {
+        echo "La ciudad 'Valledupar' no existe en el array";
+    };
+    echo "<hr>";
+
+    /**
+     * * 8) Método array_rand():
+     * * Este método es utilizado para devolver una o mas claves de forma aleatoria dentro del array.
+     */
+
+    print_r(array_rand($personas));
+    echo "<hr>";
+
+    /**
+     * ?Por ejemplo se le pueden pasar dos o más claves a retornar de forma aleatoria.
+     */
+
+    print_r(array_rand($personas,2));
+    echo "<hr>";
+
+    /**
+     * * 9) Método array_push():
+     * * Este método es utilizado para agregar uno o más elementos al final de un array.
+     */
+
+    $diego= array("Diego" => array("Edad" => 23, "Ciudad" => "Valledupar", "País" => "Colombia"));
+    array_push($personas, $diego);
+    print_r($personas);
+    echo "<hr>";
+
+    /**
+     * * 10) Método array_unique():
+     * * Este método es utilizado para eliminar valores duplicados de un array.
+     */
+
+    print_r(array_unique($personas));
+
 ?>
